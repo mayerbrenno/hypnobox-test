ea0aa207c2037766e716f1ed93d59042a8921521
Brenno Mayer <mayerbrenno@github.com>
2025-02-22 02:25:50 -0300

    Added API user registration test

--- tests/Feature/Auth/RegistrationTest.php
+++ tests/Feature/Auth/RegistrationTest.php
@@ -11,14 +11,10 @@ class RegistrationTest extends TestCase
 
     public function test_new_users_can_register(): void
     {
-        $response = $this->post('/register', [
+        $this->post('/api/register', [
             'name' => 'Test User',
             'email' => 'test@example.com',
             'password' => 'password',
-            'password_confirmation' => 'password',
-        ]);
-
-        $this->assertAuthenticated();
-        $response->assertNoContent();
+        ])->assertStatus(201);
     }
 }
